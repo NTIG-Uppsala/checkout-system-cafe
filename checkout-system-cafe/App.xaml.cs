3e4fdb7004b119e7d48e894e621a0f7622576b57
mohamadhamdan13 <142982637+mohamadhamdan13@users.noreply.github.com>
2023-12-05 10:13:15 +0100

    Add project files.

---
+++ checkout-system-cafe/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace checkout_system_cafe
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
