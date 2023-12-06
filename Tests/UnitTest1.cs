2cbdec55500d6a9819b8bd26c8752f0e49e1a4cf
SamuelWidlund <142985254+SamuelWidlund@users.noreply.github.com>
2023-12-06 11:39:06 +0100

    move duplicated code from tests
    Co-Authored-By: mohamadhamdan13 <142982637+mohamadhamdan13@users.noreply.github.com>
    Co-Authored-By: Viggo Sörensen <142979357+ViggoGitHub@users.noreply.github.com>
    Co-Authored-By: Georg af Wåhlberg <105862332+georg-af-w@users.noreply.github.com>

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,142 +1,123 @@
 using System.Diagnostics;
-using FlaUI.UIA3;
+using System.Reflection;
+
 using FlaUI.Core.AutomationElements;
 using FlaUI.Core.Conditions;
-using System.Reflection;
+using FlaUI.UIA3;
 namespace Tests
 {
     [TestClass]
     public class UnitTest1
     {
+        private readonly string _pathToExecutable;
+
+        public UnitTest1()
+        {
+            // Hämta sökvägen till aktuell katalog
+            string currentDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location) ?? throw new Exception();
+            _pathToExecutable = Path.Combine(currentDirectory, "..", "..", "..", "..", "checkout-system-cafe\\bin\\Debug\\net8.0-windows\\checkout-system-cafe.exe");
+        }
+
+        public Tuple<Window, ConditionFactory> StartWindowHelper()
+        {
+            // Starta applikationen
+            var app = FlaUI.Core.Application.Launch(_pathToExecutable);
+
+            // Använd UIA3Automation för att automatisera interaktion med UI
+            using var automation = new UIA3Automation();
+            // Hämta huvudfönstret för applikationen
+            var window = app.GetMainWindow(automation);
+
+            // Skapa en fabrik för villkor för att söka efter UI-element
+            ConditionFactory cf = new ConditionFactory(new UIA3PropertyLibrary());
+
+            return new Tuple<Window, ConditionFactory>(window, cf);
+        }
+
         [TestMethod]
         public void zeroAtStartTest()
         {
-            // Hämta sökvägen till aktuell katalog
-            string currentDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            (Window window, ConditionFactory cf) = StartWindowHelper();
 
-            // Starta applikationen
-            var app = FlaUI.Core.Application.Launch(Path.Combine(currentDirectory, "..", "..", "..", "..", "checkout-system-cafe\\bin\\Debug\\net8.0-windows\\checkout-system-cafe.exe"));
+            // Hitta etiketten som visar totalpriset
+            Label totalpricelabel = window.FindFirstDescendant(cf.ByAutomationId("Totalpris")).AsLabel();
 
-            // Använd UIA3Automation för att automatisera interaktion med UI
-            using (var automation = new UIA3Automation())
-            {
-                // Hämta huvudfönstret för applikationen
-                var window = app.GetMainWindow(automation);
-
-                // Skapa en fabrik för villkor för att söka efter UI-element
-                ConditionFactory cf = new ConditionFactory(new UIA3PropertyLibrary());
-
-                // Hitta etiketten som visar totalpriset
-                Label totalpricelabel = window.FindFirstDescendant(cf.ByAutomationId("Totalpris")).AsLabel();
-
-                // Kontrollera att totalpriset är "0 kr"
-                Trace.Assert(totalpricelabel.Text == "0 kr", "Could not find 0 kr");
-            }
+            // Kontrollera att totalpriset är "0 kr"
+            Trace.Assert(totalpricelabel.Text == "0 kr", "Could not find 0 kr");
         }
 
         [TestMethod]
         public void oneCoffeeTest()
         {
-            string currentDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-            var app = FlaUI.Core.Application.Launch(Path.Combine(currentDirectory, "..", "..", "..", "..", "checkout-system-cafe\\bin\\Debug\\net8.0-windows\\checkout-system-cafe.exe"));
-            using (var automation = new UIA3Automation())
-            {
-                var window = app.GetMainWindow(automation);
-                ConditionFactory cf = new ConditionFactory(new UIA3PropertyLibrary());
+            (Window window, ConditionFactory cf) = StartWindowHelper();
 
-                Button coffeebutton = window.FindFirstDescendant(cf.ByAutomationId("Kaffe")).AsButton();
-                Label totalpricelabel = window.FindFirstDescendant(cf.ByAutomationId("Totalpris")).AsLabel();
+            Button coffeebutton = window.FindFirstDescendant(cf.ByAutomationId("Kaffe")).AsButton();
+            Label totalpricelabel = window.FindFirstDescendant(cf.ByAutomationId("Totalpris")).AsLabel();
 
-                coffeebutton.Click();
-                Trace.Assert(totalpricelabel.Text == "15 kr", "Could not find 15 kr");
-            }
+            coffeebutton.Click();
+            Trace.Assert(totalpricelabel.Text == "15 kr", "Could not find 15 kr");
 
         }
 
         [TestMethod]
         public void threeCoffeeTest()
         {
-            string currentDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-            var app = FlaUI.Core.Application.Launch(Path.Combine(currentDirectory, "..", "..", "..", "..", "checkout-system-cafe\\bin\\Debug\\net8.0-windows\\checkout-system-cafe.exe"));
-            using (var automation = new UIA3Automation())
-            {
-                var window = app.GetMainWindow(automation);
-                ConditionFactory cf = new ConditionFactory(new UIA3PropertyLibrary());
+            (Window window, ConditionFactory cf) = StartWindowHelper();
 
-                Button coffeebutton = window.FindFirstDescendant(cf.ByAutomationId("Kaffe")).AsButton();
-                Label totalpricelabel = window.FindFirstDescendant(cf.ByAutomationId("Totalpris")).AsLabel();
+            Button coffeebutton = window.FindFirstDescendant(cf.ByAutomationId("Kaffe")).AsButton();
+            Label totalpricelabel = window.FindFirstDescendant(cf.ByAutomationId("Totalpris")).AsLabel();
 
-                coffeebutton.Click();
-                coffeebutton.Click();
-                coffeebutton.Click();
-                Trace.Assert(totalpricelabel.Text == "45 kr", "Total price does not work");
-            }
+            coffeebutton.Click();
+            coffeebutton.Click();
+            coffeebutton.Click();
+            Trace.Assert(totalpricelabel.Text == "45 kr", "Total price does not work");
 
         }
 
         [TestMethod]
         public void resetButtonTest()
         {
-            string currentDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-            var app = FlaUI.Core.Application.Launch(Path.Combine(currentDirectory, "..", "..", "..", "..", "checkout-system-cafe\\bin\\Debug\\net8.0-windows\\checkout-system-cafe.exe"));
-            using (var automation = new UIA3Automation())
-            {
-                var window = app.GetMainWindow(automation);
-                ConditionFactory cf = new ConditionFactory(new UIA3PropertyLibrary());
+            (Window window, ConditionFactory cf) = StartWindowHelper();
 
-                Button coffeebutton = window.FindFirstDescendant(cf.ByAutomationId("Kaffe")).AsButton();
-                Button resetbutton = window.FindFirstDescendant(cf.ByAutomationId("Nollställ")).AsButton();
-                Label totalpricelabel = window.FindFirstDescendant(cf.ByAutomationId("Totalpris")).AsLabel();
+            Button coffeebutton = window.FindFirstDescendant(cf.ByAutomationId("Kaffe")).AsButton();
+            Button resetbutton = window.FindFirstDescendant(cf.ByAutomationId("Nollställ")).AsButton();
+            Label totalpricelabel = window.FindFirstDescendant(cf.ByAutomationId("Totalpris")).AsLabel();
 
-                coffeebutton.Click();
-                coffeebutton.Click();
-                coffeebutton.Click();
-                resetbutton.Click();
-                Trace.Assert(totalpricelabel.Text == "0 kr", "Reset button does not work");
-            }
+            coffeebutton.Click();
+            coffeebutton.Click();
+            coffeebutton.Click();
+            resetbutton.Click();
+            Trace.Assert(totalpricelabel.Text == "0 kr", "Reset button does not work");
 
         }
 
         [TestMethod]
         public void resetAndCountinueTest()
         {
-            string currentDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-            var app = FlaUI.Core.Application.Launch(Path.Combine(currentDirectory, "..", "..", "..", "..", "checkout-system-cafe\\bin\\Debug\\net8.0-windows\\checkout-system-cafe.exe"));
-            using (var automation = new UIA3Automation())
-            {
-                var window = app.GetMainWindow(automation);
-                ConditionFactory cf = new ConditionFactory(new UIA3PropertyLibrary());
+            (Window window, ConditionFactory cf) = StartWindowHelper();
 
-                Button coffeebutton = window.FindFirstDescendant(cf.ByAutomationId("Kaffe")).AsButton();
-                Button resetbutton = window.FindFirstDescendant(cf.ByAutomationId("Nollställ")).AsButton();
-                Label totalpricelabel = window.FindFirstDescendant(cf.ByAutomationId("Totalpris")).AsLabel();
+            Button coffeebutton = window.FindFirstDescendant(cf.ByAutomationId("Kaffe")).AsButton();
+            Button resetbutton = window.FindFirstDescendant(cf.ByAutomationId("Nollställ")).AsButton();
+            Label totalpricelabel = window.FindFirstDescendant(cf.ByAutomationId("Totalpris")).AsLabel();
 
-                coffeebutton.Click();
-                resetbutton.Click();
-                coffeebutton.Click();
-                Trace.Assert(totalpricelabel.Text == "15 kr", "Add coffee combined with reset price does not work");
-            }
+            coffeebutton.Click();
+            resetbutton.Click();
+            coffeebutton.Click();
+            Trace.Assert(totalpricelabel.Text == "15 kr", "Add coffee combined with reset price does not work");
 
         }
 
         [TestMethod]
         public void resetWhenZeroTest()
         {
-            string currentDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-            var app = FlaUI.Core.Application.Launch(Path.Combine(currentDirectory, "..", "..", "..", "..", "checkout-system-cafe\\bin\\Debug\\net8.0-windows\\checkout-system-cafe.exe"));
-            using (var automation = new UIA3Automation())
-            {
-                var window = app.GetMainWindow(automation);
-                ConditionFactory cf = new ConditionFactory(new UIA3PropertyLibrary());
 
-                Button resetbutton = window.FindFirstDescendant(cf.ByAutomationId("Nollställ")).AsButton();
-                Label totalpricelabel = window.FindFirstDescendant(cf.ByAutomationId("Totalpris")).AsLabel();
+            (Window window, ConditionFactory cf) = StartWindowHelper();
+            Button resetbutton = window.FindFirstDescendant(cf.ByAutomationId("Nollställ")).AsButton();
+            Label totalpricelabel = window.FindFirstDescendant(cf.ByAutomationId("Totalpris")).AsLabel();
 
-                resetbutton.Click();
-                Trace.Assert(totalpricelabel.Text == "0 kr", "Reset when total is zero does not work");
-            }
+            resetbutton.Click();
+            Trace.Assert(totalpricelabel.Text == "0 kr", "Reset when total is zero does not work");
 
         }
     }
 }
-
